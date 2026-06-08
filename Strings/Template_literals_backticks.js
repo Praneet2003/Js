f34f9ccd8f7bafdcf3b8044c2d44f8e0a97bfae9
Praneet Raj <rajpraneet4@gmail.com>
2026-06-08 23:45:24 +0530

    Template literal backticks

---
+++ Strings/Template_literals_backticks.js
@@ -0,0 +1,7 @@
+const name = "Praneet";
+const age = 22;
+const greet = `Hello my name is ${name} and my age is ${age}`;
+console.log(greet);
+console.log(`the sum of 5 and 3 is ${5+3}.`);
+// Hello my name is Praneet and my age is 22
+// the sum of 5 and 3 is 8.
